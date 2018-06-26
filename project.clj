5f485c880a144427b9a4152a659633ffc5761012
Vlad Bokov <razum2um@mail.ru>
2018-06-26 15:02:04 +0700

    Update deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.apache.axis2/axis2-adb "1.6.2"]
-                 [org.apache.axis2/axis2-transport-http "1.6.2"]
-                 [org.apache.axis2/axis2-transport-local "1.6.2"]]
+                 [org.apache.axis2/axis2-adb "1.7.8"]
+                 [org.apache.axis2/axis2-transport-http "1.7.8"]
+                 [org.apache.axis2/axis2-transport-local "1.7.8"]]
   :source-paths ["src" "test"]
   :aot [clj-soap.test.core])
 
