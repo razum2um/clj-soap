67b58e4060e17c9dca4598a9b57ce075cbd246ae
Vlad Bokov <razum2um@mail.ru>
2018-06-26 15:24:08 +0700

    Version 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject razum2um/clj-soap "0.2.1"
+(defproject razum2um/clj-soap "0.2.2-SNAPSHOT"
   :description "SOAP Client and Server using Apache Axis2."
   :url "https://github.com/seancorfield/clj-soap"
   :license {:name "Eclipse Public License"
